b5ef3dcd87d68986fb63aba61301e65bee6e6c23
Ricardo Devis Agullo <ricardo.devisagullo@guestline.com>
2022-12-29 12:09:02 +0100

    fix checks

--- .bin/compile.sh
+++ .bin/compile.sh
@@ -7,8 +7,8 @@ sudo apt install -y vim nitrogen git build-essential xinit x11-xserver-utils \
 cd $HOME
 mkdir -p dwm
 cd dwm
-[[ -d ~/dwm/dwm-flexipatch ]] && git clone https://github.com/bakkeby/dwm-flexipatch.git
-[[ -f ~/dwm/st-flexipatch ]] && git clone https://github.com/bakkeby/st-flexipatch.git
+[[ ! -d ~/dwm/dwm-flexipatch ]] && git clone https://github.com/bakkeby/dwm-flexipatch.git
+[[ ! -d ~/dwm/st-flexipatch ]] && git clone https://github.com/bakkeby/st-flexipatch.git
 
 # Enable patches
 sed -i 's/BAR_POWERLINE_STATUS_PATCH 0/BAR_POWERLINE_STATUS_PATCH 1/' dwm-flexipatch/patches.h
