40a0e41bc426ef616a70d2ab864872e17df4d566
Ricardo Devis Agullo <ricardo.devisagullo@guestline.com>
2022-12-29 15:15:45 +0100

    add savefloats and attachabove patches

--- .bin/compile.sh
+++ .bin/compile.sh
@@ -23,6 +23,8 @@ cd dwm
 
 # Enable patches
 sed -i 's/BAR_POWERLINE_STATUS_PATCH 0/BAR_POWERLINE_STATUS_PATCH 1/' dwm-flexipatch/patches.h
+sed -i 's/SAVEFLOATS_PATCH 0/SAVEFLOATS_PATCH 1/' dwm-flexipatch/patches.h
+sed -i 's/ATTACHABOVE_PATCH 0/ATTACHABOVE_PATCH 1/' dwm-flexipatch/patches.h
 sed -i 's/FOCUSADJACENTTAG_PATCH 0/FOCUSADJACENTTAG_PATCH 1/' dwm-flexipatch/patches.h
 # Config
 cp -R $HOME/.config/dwm/* dwm-flexipatch
