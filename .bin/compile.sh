e0149f87e43b9a4c6d58d9d4123dff29397233c9
Ricardo Devis Agullo <ricardo.devisagullo@guestline.com>
2022-12-29 15:00:58 +0100

    enable focusadjacent tag

--- .bin/compile.sh
+++ .bin/compile.sh
@@ -23,6 +23,7 @@ cd dwm
 
 # Enable patches
 sed -i 's/BAR_POWERLINE_STATUS_PATCH 0/BAR_POWERLINE_STATUS_PATCH 1/' dwm-flexipatch/patches.h
+sed -i 's/FOCUSADJACENTTAG_PATCH 0/FOCUSADJACENTTAG_PATCH 1/' dwm-flexipatch/patches.h
 # Config
 cp -R $HOME/.config/dwm/* dwm-flexipatch
 cp -R $HOME/.config/st/* st-flexipatch
