0b288646224a53998192d2201f4ec309f147e18a
Ricardo Devis Agullo <ricardo.devisagullo@guestline.com>
2022-12-29 12:10:20 +0100

    remove extra code

--- .bin/compile.sh
+++ .bin/compile.sh
@@ -22,27 +22,3 @@ sudo make clean install
 cd ../st-flexipatch
 make
 sudo make clean install
-
-
-if [ -d "$DIR" ]; then
-  echo "DWM folder already exists"
-  exit 1
-else
-  mkdir dwm
-  cd dwm
-  git clone https://github.com/bakkeby/dwm-flexipatch.git
-  git clone https://github.com/bakkeby/st-flexipatch.git
-
-  # Enable patches
-  sed -i 's/BAR_POWERLINE_STATUS_PATCH 0/BAR_POWERLINE_STATUS_PATCH 1/' dwm-flexipatch/patches.h
-  # Config
-  cp -R $HOME/.config/dwm/* dwm-flexipatch
-
-  cd dwm-flexipatch
-  make
-  sudo make clean install
-
-  cd ../st-flexipatch
-  make
-  sudo make clean install
-fi
